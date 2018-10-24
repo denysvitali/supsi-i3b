a0969d8caf69bd69b6449867579348e37f1d5430
Denys Vitali <denys@denv.it>
2018-10-24 14:41:36 +0200

    CG: S3 - Full Cube

--- 5-sem/graphics/series/s3/main.cpp
+++ 5-sem/graphics/series/s3/main.cpp
@@ -11,17 +11,18 @@
 // #INCLUDE //
 //////////////
 
-   // GLM:
-   #define GLM_FORCE_RADIANS
-   #include <glm/glm.hpp>
-   #include <glm/gtc/matrix_transform.hpp>
-   #include <glm/gtc/type_ptr.hpp>
+// GLM:
+#define GLM_FORCE_RADIANS
 
-   // FreeGLUT:
-   #include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-   // C/C++:
-   #include <iostream>
+// FreeGLUT:
+#include <GL/freeglut.h>
+
+// C/C++:
+#include <iostream>
 
 
 
@@ -29,10 +30,13 @@
 // GLOBALS //
 /////////////
 
-   // Rotation angle:
-   float angle = 0.0f;
-   float distance = -45.0f;
-   int windowId;
+// Rotation angle:
+float angle = 0.0f;
+float angleX = 0.0f;
+float angleY = 0.0f;
+float angleZ = 0.0f;
+float distance = -45.0f;
+int windowId;
 
 
 
@@ -44,32 +48,79 @@
 /**
  * This is the main rendering routine automatically invoked by FreeGLUT.
  */
-void displayCallback()
-{
-   // Clear the screen:
-   glClearColor(1.0f, 0.6f, 0.1f, 1.0f); // RGBA components
-   glClear(GL_COLOR_BUFFER_BIT);
+void displayCallback() {
+    angleZ += 0.2f;
+    angleY -= 0.2f;
 
-   // Set a matrix to move our triangle:
-   glm::mat4 translation = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, distance));
-   glm::mat4 rotationZ = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+    // Clear the screen:
+    glClearColor(0.82f, 0.82f, 0.82f, 0.82f); // RGBA components
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
 
-   // Compute model matrix:
-   glm::mat4 f = translation * rotationZ;
+    // Set a matrix to move our triangle:
+    glm::mat4 translation = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, distance));
+    glm::mat4 rotationX = glm::rotate(glm::mat4(1.0f), glm::radians(angleX), glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::mat4 rotationY = glm::rotate(glm::mat4(1.0f), glm::radians(angleY), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 rotationZ = glm::rotate(glm::mat4(1.0f), glm::radians(angleZ), glm::vec3(0.0f, 0.0f, 1.0f));
 
-   // Set model matrix as current OpenGL matrix:
-   glLoadMatrixf(glm::value_ptr(f));
+    // Compute model matrix:
+    glm::mat4 f = translation * rotationX * rotationY * rotationY;
 
-   // Pass a triangle (object coordinates: the triangle is centered around the origin):
-   glBegin(GL_TRIANGLES);
-      glColor3f(0.0f, 0.0f, 1.0f);
-      glVertex3f(-10.0f, -10.0f, 0.0f);
-      glVertex3f(10.0f, -10.0f, 0.0f);
-      glVertex3f(0.0f, 10.0f, 0.0f);
-   glEnd();
+    // Set model matrix as current OpenGL matrix:
+    glLoadMatrixf(glm::value_ptr(f));
 
-   // Swap this context's buffer:
-   glutSwapBuffers();
+    // Pass a triangle (object coordinates: the triangle is centered around the origin):
+    glBegin(GL_TRIANGLE_STRIP);
+    glColor3f(95.0f/100, 31.4f/100, 13.3f/100);
+    glVertex3f(5.0f, -5.0f, 5.0f);
+    glVertex3f(-5.0f, -5.0f, 5.0f);
+    glVertex3f(5.0f, 5.0f, 5.0f);
+    glVertex3f(-5.0f, 5.0f, 5.0f);
+    glEnd();
+
+    glBegin(GL_TRIANGLE_STRIP);
+    glColor3f(49.8f/100, 72.9f/100, 0.0f/100);
+    glVertex3f(5.0f, -5.0f, -5.0f);
+    glVertex3f(-5.0f, -5.0f, -5.0f);
+    glVertex3f(5.0f, 5.0f, -5.0f);
+    glVertex3f(-5.0f, 5.0f, -5.0f);
+    glEnd();
+
+    glBegin(GL_TRIANGLE_STRIP);
+    glColor3f(0.0f/100, 64.3f/100, 93.7f/100);
+    glVertex3f(5.0f, -5.0f, -5.0f);
+    glVertex3f(5.0f, 5.0f, -5.0f);
+    glVertex3f(5.0f, -5.0f, 5.0f);
+    glVertex3f(5.0f, 5.0f, 5.0f);
+    glEnd();
+
+    glBegin(GL_TRIANGLE_STRIP);
+    glColor3f(100.0f/100, 72.5f/100, 0.0f/100);
+    glVertex3f(-5.0f, -5.0f, -5.0f);
+    glVertex3f(-5.0f, 5.0f, -5.0f);
+    glVertex3f(-5.0f, -5.0f, 5.0f);
+    glVertex3f(-5.0f, 5.0f, 5.0f);
+    glEnd();
+
+    glBegin(GL_TRIANGLE_STRIP);
+    glColor3f(28.6f/100, 28.6f/100, 28.6f/100);
+    glVertex3f(-5.0f, -5.0f, -5.0f);
+    glVertex3f(5.0f, -5.0f, -5.0f);
+    glVertex3f(-5.0f, -5.0f, 5.0f);
+    glVertex3f(5.0f, -5.0f, 5.0f);
+    glEnd();
+
+
+    glBegin(GL_TRIANGLE_STRIP);
+    glColor3f(72.3f/100, 0.0f/100, 0.0f/100);
+    glVertex3f(-5.0f, 5.0f, -5.0f);
+    glVertex3f(5.0f, 5.0f, -5.0f);
+    glVertex3f(-5.0f, 5.0f, 5.0f);
+    glVertex3f(5.0f, 5.0f, 5.0f);
+    glEnd();
+    // Swap this context's buffer:
+    glutPostWindowRedisplay(windowId);
+    glutSwapBuffers();
 }
 
 
@@ -79,17 +130,15 @@ void displayCallback()
  * @param width new window width
  * @param height new window height
  */
-void reshapeCallback(int width, int height)
-{
-   std::cout << "[reshape func invoked]" << std::endl;
+void reshapeCallback(int width, int height) {
+    std::cout << "[reshape func invoked]" << std::endl;
 
-   glViewport(0, 0, width, height);
-   glMatrixMode(GL_PROJECTION);
-   glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) width / (float) height, 1.0f, 100.0f);
-   glLoadMatrixf(glm::value_ptr(projection));
-   glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) width / (float) height, 1.0f, 100.0f);
+    glLoadMatrixf(glm::value_ptr(projection));
+    glMatrixMode(GL_MODELVIEW);
 }
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
@@ -98,32 +147,30 @@ void reshapeCallback(int width, int height)
  * @param mouseX mouse X coordinate
  * @param mouseY mouse Y coordinate
  */
-void specialCallback(int key, int mouseX, int mouseY)
-{
-   std::cout << "[key pressed]" << std::endl;
+void specialCallback(int key, int mouseX, int mouseY) {
+    std::cout << "[key pressed]" << std::endl;
 
-   const float speed = 0.5f;
-   switch (key)
-   {
-      case GLUT_KEY_UP:
-         distance -= speed;
-         break;
+    const float speed = 2.0f;
+    switch (key) {
+        case GLUT_KEY_UP:
+            distance -= speed;
+            break;
 
-      case GLUT_KEY_DOWN:
-         distance += speed;
-         break;
+        case GLUT_KEY_DOWN:
+            distance += speed;
+            break;
 
-      case GLUT_KEY_LEFT:
-         angle += speed;
-         break;
+        case GLUT_KEY_LEFT:
+            angleX += speed;
+            break;
 
-      case GLUT_KEY_RIGHT:
-         angle -= speed;
-         break;
-   }
+        case GLUT_KEY_RIGHT:
+            angleX -= speed;
+            break;
+    }
 
-   // Force rendering refresh:
-   glutPostWindowRedisplay(windowId);
+    // Force rendering refresh:
+    glutPostWindowRedisplay(windowId);
 }
 
 
@@ -138,56 +185,55 @@ void specialCallback(int key, int mouseX, int mouseY)
  * @param argv array containing up to argc passed arguments
  * @return error code (0 on success, error code otherwise)
  */
-int main(int argc, char *argv[])
-{
-   // Credits:
-   std::cout << "Advanced FreeGLUT example - A. Peternier (C) SUPSI" << std::endl;
-   std::cout << std::endl;
+int main(int argc, char *argv[]) {
+    // Credits:
+    std::cout << "Advanced FreeGLUT example - A. Peternier (C) SUPSI" << std::endl;
+    std::cout << std::endl;
 
-   // Init context:
-   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-   glutInitWindowPosition(100, 100);
+    // Init context:
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowPosition(100, 100);
 
-   // FreeGLUT can parse command-line params, in case:
-   glutInit(&argc, argv);
+    // FreeGLUT can parse command-line params, in case:
+    glutInit(&argc, argv);
 
-   // Set some optional flags:
-   glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+    // Set some optional flags:
+    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
 
-   // Create the window with a specific title:
-   windowId = glutCreateWindow("FreeGLUT advanced example");
+    // Create the window with a specific title:
+    windowId = glutCreateWindow("FreeGLUT advanced example");
 
-   // The OpenGL context is now initialized...
+    // The OpenGL context is now initialized...
 
-   // Set callback functions:
-   glutDisplayFunc(displayCallback);
-   glutReshapeFunc(reshapeCallback);
-   glutSpecialFunc(specialCallback);
+    // Set callback functions:
+    glutDisplayFunc(displayCallback);
+    glutReshapeFunc(reshapeCallback);
+    glutSpecialFunc(specialCallback);
 
-   // Check OpenGL version:
-   std::cout << "OpenGL context" << std::endl;
-   std::cout << "   version  . . : " << glGetString(GL_VERSION) << std::endl;
-   std::cout << "   vendor . . . : " << glGetString(GL_VENDOR) << std::endl;
-   std::cout << "   renderer . . : " << glGetString(GL_RENDERER) << std::endl;
-   std::cout << "   extensions . : " << glGetString(GL_EXTENSIONS) << std::endl;
+    // Check OpenGL version:
+    std::cout << "OpenGL context" << std::endl;
+    std::cout << "   version  . . : " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "   vendor . . . : " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "   renderer . . : " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "   extensions . : " << glGetString(GL_EXTENSIONS) << std::endl;
 
-   // Additional context info:
-   std::cout << "   color bit  . : " << glutGet(GLUT_WINDOW_BUFFER_SIZE)
-                                     << " (red: " << glutGet(GLUT_WINDOW_RED_SIZE)
-                                     << ", green: " << glutGet(GLUT_WINDOW_GREEN_SIZE)
-                                     << ", blue: " << glutGet(GLUT_WINDOW_BLUE_SIZE)
-                                     << ", alpha: " << glutGet(GLUT_WINDOW_ALPHA_SIZE)
-                                     << ")" << std::endl;
-   int bits = 0;
-   glGetIntegerv(GL_DEPTH_BITS, &bits);
-   std::cout << "   depth bit  . : " << glutGet(GLUT_WINDOW_DEPTH_SIZE) << " (max " << bits << ")" <<  std::endl;
-   std::cout << "   stencil bit  : " << glutGet(GLUT_WINDOW_STENCIL_SIZE) << std::endl;
-   std::cout << "   double buff. : " << glutGet(GLUT_WINDOW_DOUBLEBUFFER) << std::endl;
+    // Additional context info:
+    std::cout << "   color bit  . : " << glutGet(GLUT_WINDOW_BUFFER_SIZE)
+              << " (red: " << glutGet(GLUT_WINDOW_RED_SIZE)
+              << ", green: " << glutGet(GLUT_WINDOW_GREEN_SIZE)
+              << ", blue: " << glutGet(GLUT_WINDOW_BLUE_SIZE)
+              << ", alpha: " << glutGet(GLUT_WINDOW_ALPHA_SIZE)
+              << ")" << std::endl;
+    int bits = 0;
+    glGetIntegerv(GL_DEPTH_BITS, &bits);
+    std::cout << "   depth bit  . : " << glutGet(GLUT_WINDOW_DEPTH_SIZE) << " (max " << bits << ")" << std::endl;
+    std::cout << "   stencil bit  : " << glutGet(GLUT_WINDOW_STENCIL_SIZE) << std::endl;
+    std::cout << "   double buff. : " << glutGet(GLUT_WINDOW_DOUBLEBUFFER) << std::endl;
 
-   // Enter the main FreeGLUT processing loop:
-   glutMainLoop();
+    // Enter the main FreeGLUT processing loop:
+    glutMainLoop();
 
-   // Done:
-   std::cout << "[application terminated]" << std::endl;
-   return 0;
+    // Done:
+    std::cout << "[application terminated]" << std::endl;
+    return 0;
 }
